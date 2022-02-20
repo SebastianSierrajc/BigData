206cb798b0ac11e50a8b8aa910d1b98b1a18c9e2
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2022-02-19 21:37:41 -0500

    map reduce punto 2

---
+++ corte1/map-reduce/code/punto_2/mapper.py
@@ -0,0 +1,28 @@
+import sys
+import re
+
+
+ID = 'Transaction unique identifier'
+PRICE = 'Price'
+DATE = 'Date of Transfer'
+TYPE = 'Property Type'
+STATE = 'Old/New'
+DURATION = 'Duration'
+CITY = 'Town/City'
+DISTRICT = 'District'
+COUNTY = 'County'
+PPD = 'PPDCategory Type'
+RECORD = 'Record Status - monthly file only'
+
+header = {}
+
+for l, line in enumerate(sys.stdin):
+    words = line.strip('\n').split(',')
+    if l == 0:
+        for i, word in enumerate(words):
+            header[word] = i
+        continue 
+
+    price, city = words[header[PRICE]], words[header[CITY]].strip()
+    city = re.sub("[^\w\s]", "", city)
+    print(f"{city}\t{price}")
---
+++ corte1/map-reduce/code/punto_2/reducer.py
@@ -0,0 +1,19 @@
+import sys
+
+current_k = None
+current_v = 0
+current_c = 0
+
+for line in sys.stdin:
+    key, value = line.strip('\n').split('\t')
+    if current_k == key:
+        current_c += 1
+        current_v += int(value)
+    else:
+        if current_k != None:
+            print(f"{current_k}\t{current_v / current_c}")
+        current_k = key
+        current_v = int(value)
+        current_c = 1
+
+print(f"{current_k}\t{current_v / current_c}")
