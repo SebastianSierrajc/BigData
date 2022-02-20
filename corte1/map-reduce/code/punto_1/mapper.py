db1d8d9138c051bfb177875b6ec3b9d0c3d1ba54
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2022-02-19 21:37:13 -0500

    adding const for cols names

--- corte1/map-reduce/code/punto_1/mapper.py
+++ corte1/map-reduce/code/punto_1/mapper.py
@@ -1,9 +1,27 @@
 import sys
 import re
 
+ID = 'Transaction unique identifier'
+PRICE = 'Price'
+DATE = 'Date of Transfer'
+TYPE = 'Property Type'
+STATE = 'Old/New'
+DURATION = 'Duration'
+CITY = 'Town/City'
+DISTRICT = 'District'
+COUNTY = 'County'
+PPD = 'PPDCategory Type'
+RECORD = 'Record Status - monthly file only'
+
+header = {}
+
+
 for l, line in enumerate(sys.stdin):
     words = line.strip('\n').split(',')
-    if l == 0: continue 
-    date = words[2]
+    if l == 0:
+        for i, word in enumerate(words):
+            header[word] = i
+        continue 
+    date = words[header[DATE]]
     year = date.split('-')[0]
     print(f"{year}\t{1}")
