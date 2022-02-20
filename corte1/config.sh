1f2430efe1789ef2b913e9bcc086b5dd7eb12c89
SebastianSierrajc <sebastian.sierrajc@gmail.com>
2022-02-20 21:50:29 +0000

    update config file

--- corte1/config.sh
+++ corte1/config.sh
@@ -1,7 +1,20 @@
-echo "coping map-reduce jar"
-cp /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar hadoop-streaming.jar
+echo "installing and updating"
+sudo apt install unzip vim
+sudo apt update
+sudo apt upgrade
 
-echo "Downloading dataset"
+echo "coping map-reduce jar\n"
+cp /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar ~/hadoop-streaming.jar
+
+echo "Downloading dataset\n"
 wget -P ~ https://objectstorage.us-ashburn-1.oraclecloud.com/n/idvvpwer9msp/b/bucket-20220209-1358/o/archive.zip
 
+
+echo "unzip dataset\n"
+unzip ~/archive.zip
+
+echo "creating input directory in cluster\n"
 hdfs dfs -mkdir /data
+
+echo "put dataset into cluster input\n"
+hdfs dfs -put ~/price_paid_records.csv /data
