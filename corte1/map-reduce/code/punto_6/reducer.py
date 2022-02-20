08362b7f5cc6a3928d129f5641cc8c57dd6f44d7
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2022-02-20 01:18:59 -0500

    map reduce punto 6

---
+++ corte1/map-reduce/code/punto_6/reducer.py
@@ -0,0 +1,28 @@
+from calendar import c
+import sys
+
+current_co = None
+current_ci = None
+count = 0
+
+counties = {}
+
+for line in sys.stdin:
+    key, value = line.strip('\n').split('\t')
+    if key == current_co:
+        if value != current_ci:
+            count += 1
+            current_ci = value
+    else:
+        if current_co != None:
+            if count in counties:
+                counties[count] += 1
+            else:
+                counties[count] = 1
+        current_co = key
+        current_ci = value
+        count = 1
+
+
+for key, value in counties.items():
+    print("{}\t{}".format(value, key))
